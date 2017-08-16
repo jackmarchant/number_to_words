8756b5bf7c18ffa73c491e4f997a0f8ef62e2e01
Jack Marchant <jack@expert360.com>
2017-08-16 23:09:24 +1000

    number to words test

---
+++ lib/elixir_num_to_words.ex
@@ -0,0 +1,31 @@
+defmodule ElixirNumToWords do
+  @moduledoc """
+  Documentation for ElixirNumToWords.
+  """
+
+  @units ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+  @tens ["ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+  def num_to_words(num) when num <= 9 do
+    Enum.at(@units, num - 1)
+  end
+
+  def num_to_words(num) when num > 9 do
+    tens = @tens |> get_at_index(div(num, 10))
+    units = num |> get_remainder |> maybe_get_units
+
+    print_word(tens, units)
+  end
+
+  defp print_word(tens, ""), do: tens
+  defp print_word(tens, units), do: tens <> " " <> units
+
+  defp get_remainder(num), do: rem(num, 10)
+
+  defp maybe_get_units(0), do: ""
+  defp maybe_get_units(index), do: get_at_index(@units, index)
+
+  defp get_at_index(list, index) do
+    Enum.at(list, index - 1)
+  end
+end
---
+++ test/elixir_num_to_words_test.exs
@@ -0,0 +1,12 @@
+defmodule ElixirNumToWordsTest do
+  use ExUnit.Case
+  doctest ElixirNumToWords
+
+  test "can convert number to words" do
+    words = [{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {20, "twenty"}, {21, "twenty one"}, {22, "twenty two"}, {23, "twenty three"}, {30, "thirty"}, {31, "thirty one"}, {45, "fourty five"}, {72, "seventy two"}, {99, "ninety nine"}]
+
+    Enum.each(words, fn {num, word} ->
+      assert ElixirNumToWords.num_to_words(num) == word
+    end)
+  end
+end
