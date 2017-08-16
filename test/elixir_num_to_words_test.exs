b7c813ac5ca9cf5c1e031c7f4fa10fc16b168c33
Jack Marchant <jack@expert360.com>
2017-08-17 09:19:44 +1000

    use recursion

--- test/elixir_num_to_words_test.exs
+++ test/elixir_num_to_words_test.exs
@@ -3,10 +3,14 @@ defmodule ElixirNumToWordsTest do
   doctest ElixirNumToWords
 
   test "can convert number to words" do
-    words = [{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {20, "twenty"}, {21, "twenty one"}, {22, "twenty two"}, {23, "twenty three"}, {30, "thirty"}, {31, "thirty one"}, {45, "forty five"}, {72, "seventy two"}, {99, "ninety nine"}]
+    number_word_tuples = [{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {20, "twenty"}, {21, "twenty one"}, {22, "twenty two"}, {23, "twenty three"}, {30, "thirty"}, {31, "thirty one"}, {45, "forty five"}, {72, "seventy two"}, {99, "ninety nine"}]
 
-    Enum.each(words, fn {num, word} ->
-      assert ElixirNumToWords.num_to_words(num) == word
-    end)
+    loop_number_words(number_word_tuples)
+  end
+
+  def loop_number_words([]), do: nil
+  def loop_number_words([{num, word} | words]) do
+    assert ElixirNumToWords.num_to_words(num) == word
+    loop_number_words(words)
   end
 end
